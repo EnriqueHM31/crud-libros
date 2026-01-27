f7e21e87e0105d17871e52125679300792d49544
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-26 22:22:59 -0600

    Integrate book loading functionality in App component by utilizing Zustand store; implement useEffect to load books on component mount.

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,23 @@ import { useMenuStore } from "./store/menu";
 import Home from "./components/Home";
 import Libros from "./components/Libros";
 import Contacto from "./components/Contacto";
+import { useBooksStore } from "./store/libro";
+import { useEffect } from "react";
 
 function App() {
     const { currentMenu } = useMenuStore();
+    const { cargarLibros } = useBooksStore();
+
+    useEffect(() => {
+        cargarLibros();
+    },
+        [cargarLibros]);
     return (
         <main className="flex h-screen w-screen">
             <div className="flex-1">
                 <AsideNav />
             </div>
-            <div className="flex-4 p-5">
+            <div className="flex-4 p-5 overflow-y-auto h-screen">
                 {currentMenu === "home" && <Home />}
                 {currentMenu === "libros" && <Libros />}
                 {currentMenu === "contacto" && <Contacto />}
