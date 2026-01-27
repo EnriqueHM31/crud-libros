905c2861887d356825cfd3e51f8fa742787ee900
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-26 21:45:29 -0600

    Implement dynamic menu rendering in App component using Zustand store; enhance layout structure for improved user experience.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import AsideNav from "./components/AsideNav";
+import { useMenuStore } from "./store/menu";
+import Home from "./components/Home";
+import Libros from "./components/Libros";
+import Contacto from "./components/Contacto";
 
 function App() {
+    const { currentMenu } = useMenuStore();
     return (
-        <>
-            <AsideNav />
-        </>
+        <main className="flex h-screen w-screen">
+            <div className="flex-1">
+                <AsideNav />
+            </div>
+            <div className="flex-4 p-5">
+                {currentMenu === "home" && <Home />}
+                {currentMenu === "libros" && <Libros />}
+                {currentMenu === "contacto" && <Contacto />}
+            </div>
+        </main>
     );
 }
 
