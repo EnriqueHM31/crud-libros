b9af440261cc575cc4822eb0e9930d31d26ba451
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-27 21:55:37 -0600

    Add HeaderTypeFormatBook component for view mode selection in Libros; includes list and grid options with icons for improved user interface and experience.

---
+++ src/components/Libros/FormatoBooks.tsx
@@ -0,0 +1,32 @@
+import { FaList, FaThLarge } from "react-icons/fa";
+
+type ViewMode = "list" | "grid";
+interface HeaderTypeFormatBookProps {
+    viewMode: string;
+    handleViewMode: (viewMode: ViewMode) => void;
+}
+
+const typeViews = [
+    { name: "Lista", value: "list", icono: <FaList /> },
+    { name: "Grilla", value: "grid", icono: <FaThLarge /> },
+];
+
+export default function HeaderTypeFormatBook({ viewMode, handleViewMode }: HeaderTypeFormatBookProps) {
+    return (
+        <div className="flex items-center justify-between ">
+            <h2 className="text-xl font-semibold my-10">Libros</h2>
+
+            <div className="flex gap-2">
+                {typeViews.map(({ name, value, icono }: (typeof typeViews)[number]) => (
+                    <button
+                        key={name}
+                        onClick={() => handleViewMode(value as ViewMode)}
+                        className={`rounded-lg p-2 ${viewMode === value ? "bg-primary text-background" : "text-primary hover:bg-background"}`}
+                    >
+                        {icono}
+                    </button>
+                ))}
+            </div>
+        </div>
+    );
+}
