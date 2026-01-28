419f741363885be807eeea9067bcb08fe7d3fecf
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-27 21:55:33 -0600

    Refactor BooksFilters component to improve layout and styling; adjust input and button styles for better user experience, and streamline the structure for enhanced readability.

--- src/components/Libros/Filters.tsx
+++ src/components/Libros/Filters.tsx
@@ -1,4 +1,4 @@
-import { FaSearch, FaRedo, FaBook, FaUser, FaLanguage, FaFileAlt, FaFilter } from "react-icons/fa";
+import { FaSearch, FaRedo, FaBook, FaUser, FaLanguage, FaFileAlt } from "react-icons/fa";
 import { useBooksFiltersStore } from "../../store/filtros";
 import { motion } from "framer-motion";
 
@@ -6,43 +6,38 @@ export default function BooksFilters() {
     const { search, category, author, language, maxPages, setSearch, setCategory, setAuthor, setLanguage, setMaxPages, resetFilters } = useBooksFiltersStore();
 
     return (
-        <section className="border-background-secondary rounded-2xl border p-6 shadow-lg">
-            {/* Header */}
-            <div className="border-background-secondary flex items-center justify-between gap-3 border-b pb-4">
-                <div className="flex items-center gap-3">
-                    <div className="bg-primary rounded-lg p-2.5">
-                        <FaFilter className="text-lg text-white" />
-                    </div>
-                    <h2 className="text-secondary text-xl font-semibold">Filtrar libros</h2>
-                </div>
-                {/* Reset mejorado */}
-                <div className="mt-6 flex justify-end">
-                    <motion.button
-                        onClick={resetFilters}
-                        initial="hidden"
-                        animate="visible"
-                        whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
-                        whileTap={{ scale: 0.95, transition: { duration: 0.2 } }}
-                        transition={{ duration: 0.2, delay: 1.6 }}
-                        className="bg-primary text-background hover:bg-primary flex w-full cursor-pointer items-center justify-center gap-3 rounded-xl px-4 py-2 font-semibold transition-colors duration-150"
-                    >
-                        <FaRedo className="transition-transform duration-300 group-hover:rotate-180" />
-                        Limpiar filtros
-                    </motion.button>
-                </div>
-            </div>
+        <section className="border-background-secondary rounded-2xl border py-3 shadow-lg">
+
 
             <div className="space-y-4">
                 {/* Search destacado */}
-                <div className="group relative">
-                    <FaSearch className="text-text-secondary/80 group-hover:text-primary absolute top-1/2 left-4 -translate-y-1/2 transition-colors" />
-                    <input
-                        type="text"
-                        placeholder="Buscar por título o descripción..."
-                        value={search}
-                        onChange={(e) => setSearch(e.target.value)}
-                        className="w-full rounded-xl border-2 border-gray-200 bg-white px-12 py-3.5 text-sm shadow-sm transition-all hover:shadow-md focus:border-blue-500 focus:ring-4 focus:ring-blue-100 focus:outline-none"
-                    />
+                <div className="group relative flex items-center justify-between gap-6">
+                    <div className="group relative flex-5">
+                        <FaSearch className="text-text-secondary/80 group-hover:text-primary absolute top-1/2 left-4 -translate-y-1/2 transition-colors" />
+                        <input
+                            type="text"
+                            placeholder="Buscar por título o descripción..."
+                            value={search}
+                            onChange={(e) => setSearch(e.target.value)}
+                            className="w-full rounded-xl border-2 border-gray-200 bg-white px-12 py-2.5 text-sm shadow-sm transition-all hover:shadow-md focus:border-blue-500 focus:ring-4 focus:ring-blue-100 focus:outline-none"
+                        />
+                    </div>
+
+                    {/* Reset mejorado */}
+                    <div className="flex justify-end flex-1">
+                        <motion.button
+                            onClick={resetFilters}
+                            initial="hidden"
+                            animate="visible"
+                            whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
+                            whileTap={{ scale: 0.95, transition: { duration: 0.2 } }}
+                            transition={{ duration: 0.2, delay: 1.6 }}
+                            className="bg-primary text-background hover:bg-primary flex w-full cursor-pointer items-center justify-center gap-3 rounded-xl px-4 font-semibold transition-colors duration-150 py-2.5"
+                        >
+                            <FaRedo className="transition-transform duration-300 group-hover:rotate-180" />
+                            Limpiar filtros
+                        </motion.button>
+                    </div>
                 </div>
 
                 {/* Grid de filtros con iconos */}
