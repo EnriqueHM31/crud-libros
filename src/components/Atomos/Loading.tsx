1250276bfceabe882d1d54ef33a46ff5dc9419d4
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-27 22:43:36 -0600

    Enhance LoadingBooks component styling for improved dark mode support; updates include background color adjustments, icon color changes, and text visibility enhancements to ensure a better user experience across themes.

--- src/components/Atomos/Loading.tsx
+++ src/components/Atomos/Loading.tsx
@@ -3,7 +3,7 @@ import { FaBookOpen } from "react-icons/fa";
 
 export default function LoadingBooks() {
     return (
-        <div className="text-text flex h-full w-full flex-col items-center justify-center gap-6">
+        <div className=" flex h-full w-full flex-col items-center justify-center gap-6 bg-white dark:bg-primary-dark">
             {/* Libro */}
             <motion.div
                 animate={{
@@ -20,24 +20,24 @@ export default function LoadingBooks() {
                 <motion.div
                     animate={{ rotateY: [0, -50, 0] }}
                     transition={{ repeat: Infinity, duration: 2, ease: "easeInOut" }}
-                    className="bg-primary shadow-primary h-24 w-16 origin-right rounded-l-md shadow-lg"
+                    className="bg-primary dark:bg-white shadow-primary dark:shadow-white  h-24 w-16 origin-right rounded-l-md shadow-lg"
                 />
 
                 {/* Centro (lomo) */}
-                <div className="bg-surface border-secondary/80 z-10 -mx-1 flex h-24 w-6 items-center justify-center border px-1">
-                    <FaBookOpen className="text-secondary/80 text-xl" />
+                <div className="bg-surface dark:bg-primary-dark border-secondary/80 dark:border-gray-400 z-10 -mx-1 flex h-24 w-6 items-center justify-center border px-1">
+                    <FaBookOpen className="text-secondary/80 dark:text-white text-xl" />
                 </div>
 
                 {/* Portada derecha */}
                 <motion.div
                     animate={{ rotateY: [0, 50, 0] }}
                     transition={{ repeat: Infinity, duration: 2, ease: "easeInOut" }}
-                    className="bg-primary shadow-primary h-24 w-16 origin-left rounded-r-md shadow-lg"
+                    className="bg-primary dark:bg-white shadow-primary dark:shadow-white h-24 w-16 origin-left rounded-r-md shadow-lg"
                 />
             </motion.div>
 
             {/* Texto */}
-            <motion.p animate={{ opacity: [0.4, 1, 0.4] }} transition={{ repeat: Infinity, duration: 1.5 }} className="text-sm font-medium tracking-wide">
+            <motion.p animate={{ opacity: [0.4, 1, 0.4] }} transition={{ repeat: Infinity, duration: 1.5 }} className="text-sm font-medium tracking-wide text-black dark:text-white">
                 Cargando libros…
             </motion.p>
         </div>
