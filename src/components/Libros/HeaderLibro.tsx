d4ef647ffa9ec9147bce4043e70d3d684f1efe5c
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-27 21:55:42 -0600

    Add HeaderLibro component to Libros for improved layout and user interaction; includes a title, description, and animated button for creating new books.

---
+++ src/components/Libros/HeaderLibro.tsx
@@ -0,0 +1,27 @@
+import HeaderSection from "../Atomos/Header";
+import { motion } from "framer-motion";
+import { FaBook } from "react-icons/fa";
+
+export default function HeaderLibro() {
+    return (
+        < div className="flex items-center justify-between" >
+            <div className="flex-5">
+                <HeaderSection title="Administracion de libros" description="Gestiona los libros disponibles de la aplicación." />
+            </div>
+
+            <div className="flex-1">
+                <motion.button
+                    initial="hidden"
+                    animate="visible"
+                    whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
+                    whileTap={{ scale: 0.95, transition: { duration: 0.2 } }}
+                    transition={{ duration: 0.2, delay: 1.6 }}
+                    className="bg-primary text-background hover:bg-primary flex w-full cursor-pointer items-center justify-center gap-3 rounded-xl px-4 py-2.5 font-semibold transition-colors duration-150"
+                >
+                    <FaBook className="transition-transform duration-300 group-hover:rotate-180" />
+                    Crear libro
+                </motion.button>
+            </div>
+        </div >
+    );
+}
