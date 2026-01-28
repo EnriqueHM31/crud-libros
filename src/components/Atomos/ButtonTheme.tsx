d263baca4fdbb6a1a441e8e881b3ef1eb65dbbda
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-28 15:13:31 -0600

    Add ButtonTheme component for enhanced dark mode support; introduces a toggle switch with icons for improved user experience and visual consistency across themes.

---
+++ src/components/Atomos/ButtonTheme.tsx
@@ -0,0 +1,52 @@
+import { FaMoon } from "react-icons/fa";
+import { MdSunny } from "react-icons/md";
+import { useThemeStore } from "../../store/theme";
+import { motion } from "framer-motion";
+
+export default function ButtonTheme() {
+    const { mode, toggleMode } = useThemeStore();
+
+    const isDark = mode === "dark";
+
+    return (
+        <label className="relative inline-flex cursor-pointer items-center">
+            <input
+                type="checkbox"
+                checked={isDark}
+                onChange={toggleMode}
+                className="peer sr-only"
+            />
+
+            <motion.div
+                initial="hidden"
+                animate="visible"
+                whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
+                whileTap={{ scale: 0.95, transition: { duration: 0.2 } }}
+                transition={{ duration: 0.2, delay: 1.6 }}
+                className="
+                    relative h-9 w-16 rounded-full border-2 
+                     bg-none border-gray-500 shadow-md 
+                    transition-colors duration-500
+                    dark:border-gray-400 dark:bg-primary-dark
+                    peer-focus:outline-none
+                "
+            >
+                {/* Switch */}
+                <span
+                    className={`
+        absolute top-1 left-1 size-6 rounded-full 
+         transition-transform duration-300
+        peer-checked:translate-x-8
+        dark:bg-blue-600 z-10 ${isDark ? "translate-x-7 " : "translate-x-0 bg-white "}
+    `}
+                />
+
+
+
+                {/* Icons */}
+                <MdSunny className="absolute left-1 top-1 size-6 text-yellow-400" />
+                <FaMoon className="absolute right-2 top-2 size-4 text-gray-300" />
+            </motion.div>
+        </label>
+    );
+}
