7f12302a69e93311ca1984e180db408a3c7e49f9
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-28 18:54:16 -0600

    Refactor App component to streamline routing; replaced manual menu handling with PagesRoutes for improved navigation and code simplicity.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
-import AsideNav from "./components/AsideNav";
-import { useMenuStore } from "./store/menu";
-import Home from "./components/Home";
-import Libros from "./components/Libros";
-import Contacto from "./components/Contacto";
-import { useBooksStore } from "./store/libro";
-import { useEffect } from "react";
-
+import { PagesRoutes } from "./routes/pages.routes";
 function App() {
-    const { currentMenu } = useMenuStore();
-    const { cargarLibros } = useBooksStore();
-
-    useEffect(() => {
-        cargarLibros();
-    }, [cargarLibros]);
     return (
-        <main className="flex h-screen w-screen">
-            <div className="flex-1">
-                <AsideNav />
-            </div>
-            <div className="h-screen flex-4 overflow-y-auto">
-                {currentMenu === "home" && <Home />}
-                {currentMenu === "libros" && <Libros />}
-                {currentMenu === "contacto" && <Contacto />}
-            </div>
-        </main>
+        <div className="min-h-screen w-full">
+            <PagesRoutes />
+        </div>
     );
 }
 
