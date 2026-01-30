f5b9df7219a7ecd819c06173daeb477f02b584ba
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-29 21:14:58 -0600

    Update InputForm component styling by removing redundant class names for improved clarity and consistency in input rendering.

--- src/components/Libros/Atomos/InputForm.tsx
+++ src/components/Libros/Atomos/InputForm.tsx
@@ -9,7 +9,7 @@ export default function InputForm({
             <label className="text-primary text-sm font-medium dark:text-gray-400">{label}</label>
             <input
                 {...props}
-                className="bg-background text-primary-dark mt-1 w-full rounded-xl border px-3 py-2 dark:border-white/10 dark:bg-white/5 dark:text-white focus:outline-blue-600 "
+                className="bg-background text-primary-dark mt-1 w-full rounded-xl border px-3 py-2 focus:outline-blue-600 dark:border-white/10 dark:bg-white/5 dark:text-white"
             />
         </div>
     );
