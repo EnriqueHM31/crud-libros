e570f761cae7cbeca1a9677aaa83fd43abc8e794
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-29 23:25:36 -0600

    Refactor HeaderTypeFormatBook component layout for improved responsiveness; adjust flex properties and class order for better alignment and visual consistency across screen sizes.

--- src/components/Dashboard/Libros/FormatoBooks.tsx
+++ src/components/Dashboard/Libros/FormatoBooks.tsx
@@ -15,20 +15,23 @@ const typeViews = [
 export default function HeaderTypeFormatBook({ viewMode, handleViewMode }: HeaderTypeFormatBookProps) {
     const { total } = useFilteredBooks();
     return (
-        <div className="flex flex-col items-center justify-between md:flex-row">
-            <h2 className="my-10 text-3xl font-bold text-black dark:text-white">Libros</h2>
+        <div className="flex items-center  w-full  justify-between md:flex-row my-6 md:mt-0">
+            <h2 className="my-0 md:my-10 text-3xl font-bold text-black dark:text-white">Libros</h2>
 
-            <div className="flex items-center gap-2">
-                <span className="me-10 text-sm font-medium text-gray-500 dark:text-gray-400">Mostrando {total} libros</span>
-                {typeViews.map(({ name, value, icono }: (typeof typeViews)[number]) => (
-                    <button
-                        key={name}
-                        onClick={() => handleViewMode(value as ViewMode)}
-                        className={`cursor-pointer rounded-lg p-2 ${viewMode === value ? "bg-primary text-background dark:bg-blue-600" : "text-primary hover:bg-background dark:text-white dark:hover:bg-blue-800"}`}
-                    >
-                        {icono}
-                    </button>
-                ))}
+            <div className="flex items-center gap-2 justify-end  w-full">
+                <span className="md:me-10 text-sm font-medium text-gray-500 dark:text-gray-400 order-2 md:order-1">Mostrando {total} libros</span>
+                <div className="flex items-center gap-2 order-1 md:order-2">
+
+                    {typeViews.map(({ name, value, icono }: (typeof typeViews)[number]) => (
+                        <button
+                            key={name}
+                            onClick={() => handleViewMode(value as ViewMode)}
+                            className={`cursor-pointer rounded-lg p-2 ${viewMode === value ? "bg-primary text-background dark:bg-blue-600" : "text-primary hover:bg-background dark:text-white dark:hover:bg-blue-800"}`}
+                        >
+                            {icono}
+                        </button>
+                    ))}
+                </div>
             </div>
         </div>
     );
