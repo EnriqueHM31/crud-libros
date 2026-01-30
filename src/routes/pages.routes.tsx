a5d0e21a8dc4b2c72ef5f570078f8fa1d633be2c
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-29 21:22:05 -0600

    Refactor PagesRoutes component to use React.lazy for Dashboard and NotFound imports, enhancing performance with code-splitting and adding a loading fallback during component loading.

--- src/routes/pages.routes.tsx
+++ src/routes/pages.routes.tsx
@@ -1,14 +1,21 @@
 import { Route, Routes } from "react-router-dom";
-import Dashboard from "../pages/Dashboard";
-import NotFound from "../pages/NotFound";
 import LoginDemo from "../pages/LoginDemo";
+import { lazy, Suspense } from "react";
+import LoadingBooks from "../components/Atomos/Loading";
+
+const Dashboard = lazy(() => import("../pages/Dashboard"));
+const NotFound = lazy(() => import("../pages/NotFound"));
 
 export function PagesRoutes() {
     return (
-        <Routes>
-            <Route path="/" element={<LoginDemo />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={
+            <div className="flex items-center justify-center min-h-screen bg-white dark:bg-primary-dark"><LoadingBooks />.</div>
+        }>
+            <Routes>
+                <Route path="/" element={<LoginDemo />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Suspense>
     );
 }
