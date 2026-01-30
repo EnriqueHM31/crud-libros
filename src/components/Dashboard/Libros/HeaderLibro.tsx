918aba6b7c295afeeb6052295281f433ce842c92
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-29 23:06:13 -0600

    Refactor HeaderLibro component layout for enhanced responsiveness; adjust flex properties and button styles for improved alignment and visual consistency across screen sizes.

--- src/components/Dashboard/Libros/HeaderLibro.tsx
+++ src/components/Dashboard/Libros/HeaderLibro.tsx
@@ -6,8 +6,8 @@ import { useBooksStore } from "@/store/libro";
 export default function HeaderLibro() {
     const { openCreateBook } = useBooksStore();
     return (
-        <div className="flex items-center justify-between flex-col md:flex-row">
-            <div className="flex-5 flex items-center justify-center">
+        <div className="flex flex-col items-center justify-between md:flex-row">
+            <div className="flex flex-5 items-center justify-center">
                 <HeaderSection title="Administracion de libros" description="Gestiona los libros disponibles de la aplicación." />
             </div>
 
@@ -19,7 +19,7 @@ export default function HeaderLibro() {
                     whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
                     whileTap={{ scale: 0.95, transition: { duration: 0.2 } }}
                     transition={{ duration: 0.2, delay: 1.6 }}
-                    className="bg-primary text-background hover:bg-primary flex w-full cursor-pointer items-center justify-center gap-3 rounded-xl px-4 py-2 md:py-2.5 font-semibold transition-colors duration-150 dark:bg-blue-600 dark:hover:bg-blue-800"
+                    className="bg-primary text-background hover:bg-primary flex w-full cursor-pointer items-center justify-center gap-3 rounded-xl px-4 py-2 font-semibold transition-colors duration-150 md:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-800"
                 >
                     <FaBook className="transition-transform duration-300 group-hover:rotate-180" />
                     Crear libro
