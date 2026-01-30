c8d65731ab2287af00e0e5bf9083b7114145173c
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-29 21:14:14 -0600

    Add InputForm component for reusable input fields with label support; enhances form consistency across the application.

---
+++ src/components/Libros/Atomos/InputForm.tsx
@@ -0,0 +1,16 @@
+export default function InputForm({
+    label,
+    ...props
+}: React.InputHTMLAttributes<HTMLInputElement> & {
+    label: string;
+}) {
+    return (
+        <div>
+            <label className="text-primary text-sm font-medium dark:text-gray-400">{label}</label>
+            <input
+                {...props}
+                className="bg-background text-primary-dark mt-1 w-full rounded-xl border px-3 py-2 dark:border-white/10 dark:bg-white/5 dark:text-white focus:outline-blue-600 "
+            />
+        </div>
+    );
+}
