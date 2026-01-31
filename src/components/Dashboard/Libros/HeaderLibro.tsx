928d94148fe7e2a836a6a03fa27b2ef94aa7d8a0
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-30 20:30:16 -0600

    Refactor HeaderLibro component layout for improved responsiveness; adjust class properties to enhance spacing and alignment across screen sizes.

--- src/components/Dashboard/Libros/HeaderLibro.tsx
+++ src/components/Dashboard/Libros/HeaderLibro.tsx
@@ -19,7 +19,7 @@ export default function HeaderLibro() {
                     whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
                     whileTap={{ scale: 0.95, transition: { duration: 0.2 } }}
                     transition={{ duration: 0.2, delay: 1.6 }}
-                    className="bg-primary text-background hover:bg-primary flex w-full cursor-pointer items-center justify-center gap-3 rounded-xl px-4 py-2 font-semibold transition-colors duration-150 md:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-800 mt-5 md:mt-0"
+                    className="bg-primary text-background hover:bg-primary mt-5 flex w-full cursor-pointer items-center justify-center gap-3 rounded-xl px-4 py-2 font-semibold transition-colors duration-150 md:mt-0 md:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-800"
                 >
                     <FaBook className="transition-transform duration-300 group-hover:rotate-180" />
                     Crear libro
