c643e27d5d7071ccc3960ce49555633613238efa
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-30 20:30:11 -0600

    Refactor HeaderTypeFormatBook component layout for improved responsiveness; adjust class properties for better alignment and spacing across screen sizes.

--- src/components/Dashboard/Libros/FormatoBooks.tsx
+++ src/components/Dashboard/Libros/FormatoBooks.tsx
@@ -15,13 +15,12 @@ const typeViews = [
 export default function HeaderTypeFormatBook({ viewMode, handleViewMode }: HeaderTypeFormatBookProps) {
     const { total } = useFilteredBooks();
     return (
-        <div className="flex items-center  w-full  justify-between md:flex-row my-6 md:mt-0">
-            <h2 className="my-0 md:my-10 text-3xl font-bold text-black dark:text-white">Libros</h2>
+        <div className="my-6 flex w-full items-center justify-between md:mt-0 md:flex-row">
+            <h2 className="my-0 text-3xl font-bold text-black md:my-10 dark:text-white">Libros</h2>
 
-            <div className="flex items-center gap-2 justify-end  w-full">
-                <span className="md:me-10 text-sm font-medium text-gray-500 dark:text-gray-400 order-2 md:order-1">Mostrando {total} libros</span>
-                <div className="flex items-center gap-2 order-1 md:order-2">
-
+            <div className="flex w-full items-center justify-end gap-2">
+                <span className="order-2 text-sm font-medium text-gray-500 md:order-1 md:me-10 dark:text-gray-400">Mostrando {total} libros</span>
+                <div className="order-1 flex items-center gap-2 md:order-2">
                     {typeViews.map(({ name, value, icono }: (typeof typeViews)[number]) => (
                         <button
                             key={name}
