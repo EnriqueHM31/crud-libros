9fd5719b8d935b394f0a8ae9a7d31c21374e55da
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-31 21:20:34 -0600

    Integrate Toaster component for enhanced notifications in App; utilize theme from useThemeStore for dynamic styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import { PagesRoutes } from "./routes/pages.routes";
+import { Toaster } from "sonner";
+import { useThemeStore } from "./store/theme";
 function App() {
+    const { mode } = useThemeStore();
     return (
         <div className="min-h-screen w-full">
             <PagesRoutes />
+
+            <Toaster
+                position="top-right"
+                toastOptions={{
+                    className: "bg-primary text-white dark:bg-blue-600 dark:text-white",
+                }}
+                duration={3000}
+                richColors={true}
+                theme={mode}
+            />
         </div>
     );
 }
