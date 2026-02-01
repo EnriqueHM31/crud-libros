64ddbb1e009ddd9ce37145603bb79a51ebb6dc8a
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-01 14:12:52 -0600

    Update InputForm component to include 'htmlFor' attribute on label for improved accessibility and proper association with input elements.

--- src/components/Dashboard/Libros/Atomos/InputForm.tsx
+++ src/components/Dashboard/Libros/Atomos/InputForm.tsx
@@ -6,7 +6,9 @@ export default function InputForm({
 }) {
     return (
         <div>
-            <label className="text-primary text-sm font-medium dark:text-gray-400">{label}</label>
+            <label className="text-primary text-sm font-medium dark:text-gray-400" htmlFor={props.id}>
+                {label}
+            </label>
             <input
                 {...props}
                 className="bg-background text-primary-dark mt-1 w-full rounded-xl border px-3 py-2 focus:outline-blue-600 dark:border-white/10 dark:bg-white/5 dark:text-white"
