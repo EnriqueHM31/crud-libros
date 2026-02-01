0b42e81bec50d96e6b8275aba1c2bd8ae1059dff
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-31 20:47:01 -0600

    Add book service with CRUD operations; implement functions to get, create, update, and delete books from the API.

---
+++ src/services/books.service.ts
@@ -0,0 +1,83 @@
+import type { GoogleBook } from "@/types/libro";
+
+export const getAllBooks = async () => {
+
+    try {
+        const response = await fetch(import.meta.env.VITE_API_URL);
+
+        if (!response.ok) {
+            throw new Error('Error al obtener libros de la API');
+        }
+        const { data } = await response.json();
+        return { data: data.items };
+    } catch (error) {
+        throw new Error('Error al obtener libros de la API' + error);
+    }
+};
+
+
+export const getBookById = async (id: string) => {
+    try {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/${id}`);
+        if (!response.ok) {
+            throw new Error('Error al obtener libro de la API');
+        }
+        const { items } = await response.json();
+        return { data: items[0] };
+    } catch (error) {
+        throw new Error('Error al obtener libro de la API' + error);
+    }
+};
+
+export const createBook = async (book: GoogleBook) => {
+    try {
+        const response = await fetch(import.meta.env.VITE_API_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(book),
+        });
+        if (!response.ok) {
+            throw new Error('Error al crear libro de la API');
+        }
+        const { items } = await response.json();
+        return { data: items[0] };
+    } catch (error) {
+        throw new Error('Error al crear libro de la API' + error);
+    }
+};
+
+export const updateBook = async (book: Partial<GoogleBook>, id: string) => {
+    try {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(book),
+        });
+        if (!response.ok) {
+            throw new Error('Error al actualizar libro de la API');
+        }
+        const { items } = await response.json();
+        return { data: items[0] };
+    } catch (error) {
+        throw new Error('Error al actualizar libro de la API' + error);
+    }
+};
+
+export const deleteBook = async (id: string) => {
+    try {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/${id}`, {
+            method: 'DELETE',
+        });
+        if (!response.ok) {
+            throw new Error('Error al eliminar libro de la API');
+        }
+        const { items } = await response.json();
+        return { data: items[0] };
+    } catch (error) {
+        throw new Error('Error al eliminar libro de la API' + error);
+    }
+};
