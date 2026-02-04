de24ced0a0653c984bc94feeb794118cffa03a3b
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-04 11:21:14 -0600

    refactor: replace hardcoded menu items with imported constants for improved maintainability

--- src/store/menu.store.ts
+++ src/store/menu.store.ts
@@ -1,18 +1,12 @@
+import { menuItems } from "@/constants/menu";
 import type { MenuState } from "@/types/store";
-import { FaBook, FaHome, FaInfo, FaShoppingCart, FaTags } from "react-icons/fa";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 export const useMenuStore = create<MenuState>()(
     persist(
         (set, get) => ({
-            menuItems: [
-                { key: "home", name: "Home", icon: FaHome },
-                { key: "informacion", name: "Información", icon: FaInfo },
-                { key: "categorias", name: "Categorias", icon: FaTags },
-                { key: "libros", name: "Libros", icon: FaBook },
-                { key: "pedidos", name: "Pedidos", icon: FaShoppingCart },
-            ],
+            menuItems: menuItems,
 
             currentMenu: "home",
             isOpen: true,
