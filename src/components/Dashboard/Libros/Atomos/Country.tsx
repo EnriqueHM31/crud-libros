a1cde6ed930f7a87e9f97b2357f013b5203d07df
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-04 14:56:55 -0600

    refactor: enhance CountrySvg component layout and improve error message display for better user experience

--- src/components/Dashboard/Libros/Atomos/Country.tsx
+++ src/components/Dashboard/Libros/Atomos/Country.tsx
@@ -5,17 +5,17 @@ export default function CountrySvg({ value, size = { width: 40, height: 20 } }: 
     const { isErrored, handleError, flagUrl } = useCountryImg(value ?? "");
 
     return (
-        <div className="flex h-full flex-col justify-between gap-2 flex-1">
+        <div className="flex h-full flex-1 flex-col justify-between gap-2">
             <span className="text-primary-soft text-sm font-medium dark:text-gray-400">Bandera</span>
 
             {isErrored ? (
-                <span className="flex h-full items-center justify-center md:justify-start text-sm font-medium text-red-500 dark:text-gray-400 min-h-10">
-                    {value ? ("No existe la bandera para " + value) : ""}
+                <span className="flex h-full min-h-10 items-center justify-center text-sm font-medium text-red-500 md:justify-start dark:text-gray-400">
+                    {value ? "No existe la bandera para " + value : ""}
                 </span>
             ) : (
-                <div className="flex h-full w-full items-center md:justify-start justify-center">
-                    {
-                        flagUrl !== "" ? <img
+                <div className="flex h-full w-full items-center justify-center md:justify-start">
+                    {flagUrl !== "" ? (
+                        <img
                             key={flagUrl} // fuerza remount al cambiar value
                             src={flagUrl}
                             alt={`Bandera de ${value}`}
@@ -23,9 +23,10 @@ export default function CountrySvg({ value, size = { width: 40, height: 20 } }: 
                             height={size.height}
                             className="text-primary text-sm dark:border-white/10 dark:text-white/60"
                             onError={handleError}
-                        /> : <div className="w-full h-full bg-primary-dark text-white text-sm font-medium">Sin bandera</div>
-                    }
-
+                        />
+                    ) : (
+                        <div className="bg-primary-dark h-full w-full text-sm font-medium text-white">Sin bandera</div>
+                    )}
                 </div>
             )}
         </div>
