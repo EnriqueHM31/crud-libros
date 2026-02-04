4056e875ef96d8fea312313afad8c300ec996be2
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-03 22:56:15 -0600

    feat: implement useSelectDinamico hook for dynamic category selection and state management

---
+++ src/hooks/useSelectDinamico.ts
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export function useSelectDinamico({ value, handleChange }: { value: string[]; handleChange: (value: string[]) => void; }) {
+    const [selectedCategories, setSelectedCategories] = useState<string[]>(value ?? []);
+    const [inputValue, setInputValue] = useState<string>(value?.join(", ") ?? "");
+
+    const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const value = e.target.value;
+
+        console.log({ value });
+        if (!value) return;
+
+        // Si ya está en la lista, no hacemos nada
+        if (selectedCategories.includes(value)) return;
+        console.log({ selectedCategories });
+
+        const newSelected = [...selectedCategories, value];
+        console.log({ newSelected });
+        setSelectedCategories(newSelected);
+        handleChange(newSelected);
+        setInputValue(newSelected.join(", "));
+    };
+
+    const handleRemoveCategory = (cat: string) => {
+        const newSelected = selectedCategories.filter((c) => c !== cat);
+        setSelectedCategories(newSelected);
+        setInputValue(newSelected.join(", "));
+        handleChange(newSelected);
+    };
+    return {
+        selectedCategories,
+        handleSelectChange,
+        handleRemoveCategory,
+        inputValue,
+    };
+}
