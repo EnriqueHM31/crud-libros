fe91222e6393403385232cee49e13c19f1d28f5f
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-04 22:20:59 -0600

    feat: add 'Lenguajes' menu item with corresponding icon to the menu

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -1,10 +1,12 @@
 import type { MenuItem } from "@/types/store";
 import { FaHome, FaInfo, FaTags, FaBook, FaShoppingCart } from "react-icons/fa";
+import { HiLanguage } from "react-icons/hi2";
 
 export const menuItems = [
     { key: "home", name: "Home", icon: FaHome },
     { key: "informacion", name: "Información", icon: FaInfo },
     { key: "categorias", name: "Categorias", icon: FaTags },
+    { key: "lenguajes", name: "Lenguajes", icon: HiLanguage },
     { key: "libros", name: "Libros", icon: FaBook },
     { key: "pedidos", name: "Pedidos", icon: FaShoppingCart },
 ] as MenuItem[];
