78584705b3a777cd836b7c52e6694057f207d106
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-05 23:01:02 -0600

    refactor: update HeaderLenguaje component to reflect language management terminology

--- src/components/Dashboard/Lenguajes/HeaderLenguajes.tsx
+++ src/components/Dashboard/Lenguajes/HeaderLenguajes.tsx
@@ -9,7 +9,7 @@ export default function HeaderLenguaje() {
     return (
         <div className="flex flex-col items-center justify-between md:flex-row">
             <div className="flex flex-5 items-center justify-center md:flex-1 md:items-start md:justify-start">
-                <HeaderSection title="Administracion de las categorias" description="Gestiona las categorias disponibles de la aplicación." />
+                <HeaderSection title="Administracion de los lenguajes " description="Gestiona los lenguajes disponibles para los libros" />
             </div>
 
             <div className="flex w-fit flex-1 items-center justify-end">
@@ -23,7 +23,7 @@ export default function HeaderLenguaje() {
                     className="bg-primary text-white mt-5 flex w-fit cursor-pointer items-center justify-center gap-3 rounded-xl px-4 py-2 font-semibold transition-colors duration-150 md:mt-0 md:py-2.5 dark:bg-blue-600 hover:bg-blue-800"
                 >
                     <HiLanguage className="transition-transform duration-300 group-hover:rotate-180" />
-                    Crear categoria
+                    Crear lenguaje
                 </motion.button>
             </div>
         </div>
