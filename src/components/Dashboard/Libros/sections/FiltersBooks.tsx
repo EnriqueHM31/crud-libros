f7466c9ae61e4f04a436c875212bfacfe49a6521
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-05 23:03:13 -0600

    fix: update className in FiltersBooks for improved styling and consistency across input fields and buttons

--- src/components/Dashboard/Libros/sections/FiltersBooks.tsx
+++ src/components/Dashboard/Libros/sections/FiltersBooks.tsx
@@ -14,14 +14,14 @@ export default function BooksFilters() {
                 {/* Search destacado */}
                 <div className="group relative flex items-center justify-between gap-6 pb-3">
                     <div className="group relative flex-1">
-                        <FaSearch className="text-gray-500  group-hover:text-primary absolute top-1/2 left-4 -translate-y-1/2 transition-colors dark:text-gray-600 dark:group-hover:text-gray-400" />
+                        <FaSearch className="group-hover:text-primary absolute top-1/2 left-4 -translate-y-1/2 text-gray-500 transition-colors dark:text-gray-600 dark:group-hover:text-gray-400" />
                         <input
                             type="text"
                             name="search"
                             placeholder="Buscar por título o descripción..."
                             value={search}
                             onChange={(e) => setSearch(e.target.value)}
-                            className="dark:bg-primary-dark w-full rounded-xl border-2 border-gray-300 bg-white px-12 py-2.5 text-sm text-black shadow-sm transition-all hover:shadow-md focus:ring-1 focus:ring-primary-dark focus:outline-none dark:border-gray-600 dark:text-white dark:focus:ring-gray-400"
+                            className="dark:bg-primary-dark focus:ring-primary-dark w-full rounded-xl border-2 border-gray-300 bg-white px-12 py-2.5 text-sm text-black shadow-sm transition-all hover:shadow-md focus:ring-1 focus:outline-none dark:border-gray-600 dark:text-white dark:focus:ring-gray-400"
                         />
                     </div>
 
@@ -34,7 +34,7 @@ export default function BooksFilters() {
                             whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
                             whileTap={{ scale: 0.95, transition: { duration: 0.2 } }}
                             transition={{ duration: 0.2, delay: 1.6 }}
-                            className="bg-primary text-white flex w-fit cursor-pointer items-center justify-center gap-3 rounded-xl px-4 py-2.5 font-semibold transition-colors duration-150 dark:bg-blue-600 hover:bg-blue-800"
+                            className="bg-primary flex w-fit cursor-pointer items-center justify-center gap-3 rounded-xl px-4 py-2.5 font-semibold text-white transition-colors duration-150 hover:bg-blue-800 dark:bg-blue-600"
                         >
                             <FaRedo className="transition-transform duration-300 group-hover:rotate-180" />
                             <span className="hidden md:block">Limpiar filtros</span>
@@ -46,23 +46,23 @@ export default function BooksFilters() {
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
                     {/* Categoría */}
                     <div className="group relative">
-                        <FaBook className="text-gray-500 group-hover:text-primary absolute top-1/2 left-4 -translate-y-1/2 transition-colors dark:text-gray-600 dark:group-hover:text-gray-400" />
+                        <FaBook className="group-hover:text-primary absolute top-1/2 left-4 -translate-y-1/2 text-gray-500 transition-colors dark:text-gray-600 dark:group-hover:text-gray-400" />
                         <select
                             value={category}
                             name="category"
                             onChange={(e) => setCategory(e.target.value)}
-                            className="dark:bg-primary-dark w-full cursor-pointer appearance-none rounded-xl border-2 border-gray-300 bg-white py-3 pr-4 pl-10 text-sm text-black shadow-sm transition-all hover:shadow-md focus:ring-1 focus:ring-primary-dark focus:outline-none dark:border-gray-600 dark:text-white dark:focus:ring-gray-400"
+                            className="dark:bg-primary-dark focus:ring-primary-dark w-full cursor-pointer appearance-none rounded-xl border-2 border-gray-300 bg-white py-3 pr-4 pl-10 text-sm text-black shadow-sm transition-all hover:shadow-md focus:ring-1 focus:outline-none dark:border-gray-600 dark:text-white dark:focus:ring-gray-400"
                         >
                             <option value="">Todas las categorías</option>{" "}
-                            {
-                                categorias.map((category: Categoria) => (
-                                    <option key={category.id} value={category.nombre}>{category.nombre}</option>
-                                ))
-                            }
+                            {categorias.map((category: Categoria) => (
+                                <option key={category.id} value={category.nombre}>
+                                    {category.nombre}
+                                </option>
+                            ))}
                         </select>
                         <div className="pointer-events-none absolute top-1/2 right-3 -translate-y-1/2">
                             <svg
-                                className="text-gray-500 h-4 w-4 group-hover:text-gray-600 dark:text-gray-600"
+                                className="h-4 w-4 text-gray-500 group-hover:text-gray-600 dark:text-gray-600"
                                 fill="none"
                                 stroke="currentColor"
                                 viewBox="0 0 24 24"
@@ -74,25 +74,25 @@ export default function BooksFilters() {
 
                     {/* Autor */}
                     <div className="group relative">
-                        <FaUser className="text-gray-500  group-hover:text-primary absolute top-1/2 left-4 -translate-y-1/2 transition-colors dark:text-gray-600 dark:group-hover:text-gray-400" />
+                        <FaUser className="group-hover:text-primary absolute top-1/2 left-4 -translate-y-1/2 text-gray-500 transition-colors dark:text-gray-600 dark:group-hover:text-gray-400" />
                         <input
                             type="text"
                             name="author"
                             placeholder="Autor"
                             value={author}
                             onChange={(e) => setAuthor(e.target.value)}
-                            className="dark:bg-primary-dark w-full cursor-pointer appearance-none rounded-xl border-2 border-gray-300 bg-white py-3 pr-4 pl-10 text-sm text-black shadow-sm transition-all hover:shadow-md focus:ring-1 focus:ring-primary-dark focus:outline-none dark:border-gray-600 dark:text-white dark:focus:ring-gray-400"
+                            className="dark:bg-primary-dark focus:ring-primary-dark w-full cursor-pointer appearance-none rounded-xl border-2 border-gray-300 bg-white py-3 pr-4 pl-10 text-sm text-black shadow-sm transition-all hover:shadow-md focus:ring-1 focus:outline-none dark:border-gray-600 dark:text-white dark:focus:ring-gray-400"
                         />
                     </div>
 
                     {/* Idioma */}
                     <div className="group relative">
-                        <FaLanguage className="text-gray-500  group-hover:text-primary absolute top-1/2 left-4 -translate-y-1/2 transition-colors dark:text-gray-600 dark:group-hover:text-gray-400" />
+                        <FaLanguage className="group-hover:text-primary absolute top-1/2 left-4 -translate-y-1/2 text-gray-500 transition-colors dark:text-gray-600 dark:group-hover:text-gray-400" />
                         <select
                             value={language}
                             name="language"
                             onChange={(e) => setLanguage(e.target.value)}
-                            className="dark:bg-primary-dark w-full cursor-pointer appearance-none rounded-xl border-2 border-gray-300 bg-white py-3 pr-4 pl-10 text-sm text-black shadow-sm transition-all hover:shadow-md focus:ring-1 focus:ring-primary-dark focus:outline-none dark:border-gray-600 dark:text-white dark:focus:ring-gray-400"
+                            className="dark:bg-primary-dark focus:ring-primary-dark w-full cursor-pointer appearance-none rounded-xl border-2 border-gray-300 bg-white py-3 pr-4 pl-10 text-sm text-black shadow-sm transition-all hover:shadow-md focus:ring-1 focus:outline-none dark:border-gray-600 dark:text-white dark:focus:ring-gray-400"
                         >
                             <option value="">Todos los idiomas</option>
                             <option value="es">🇪🇸 Español</option>
@@ -102,7 +102,7 @@ export default function BooksFilters() {
                         </select>
                         <div className="pointer-events-none absolute top-1/2 right-3 -translate-y-1/2">
                             <svg
-                                className="text-gray-500  h-4 w-4 group-hover:text-gray-600 dark:text-gray-600"
+                                className="h-4 w-4 text-gray-500 group-hover:text-gray-600 dark:text-gray-600"
                                 fill="none"
                                 stroke="currentColor"
                                 viewBox="0 0 24 24"
@@ -114,7 +114,7 @@ export default function BooksFilters() {
 
                     {/* Máx páginas */}
                     <div className="group relative">
-                        <FaFileAlt className="text-gray-500  group-hover:text-primary absolute top-1/2 left-3 z-10 -translate-y-1/2 transition-colors dark:text-gray-600 dark:group-hover:text-gray-400" />
+                        <FaFileAlt className="group-hover:text-primary absolute top-1/2 left-3 z-10 -translate-y-1/2 text-gray-500 transition-colors dark:text-gray-600 dark:group-hover:text-gray-400" />
                         <input
                             type="number"
                             name="maxPages"
@@ -122,7 +122,7 @@ export default function BooksFilters() {
                             placeholder="Máx páginas"
                             value={maxPages ?? ""}
                             onChange={(e) => setMaxPages(e.target.value ? Number(e.target.value) : null)}
-                            className="dark:bg-primary-dark w-full appearance-none rounded-xl border-2 border-gray-300 bg-white py-3 pr-4 pl-10 text-sm text-black shadow-sm transition-all hover:shadow-md focus:ring-1 focus:ring-primary-dark focus:outline-none dark:border-gray-600 dark:text-white dark:focus:ring-gray-400"
+                            className="dark:bg-primary-dark focus:ring-primary-dark w-full appearance-none rounded-xl border-2 border-gray-300 bg-white py-3 pr-4 pl-10 text-sm text-black shadow-sm transition-all hover:shadow-md focus:ring-1 focus:outline-none dark:border-gray-600 dark:text-white dark:focus:ring-gray-400"
                         />
                     </div>
                 </div>
