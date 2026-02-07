b99f88fc89840d3d6f9e0e98926a339d69a5c3ef
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-07 00:05:07 -0600

    refactor: streamline Layout component by consolidating child rendering and improving class name formatting

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,11 +2,9 @@ import Navbar from "@/components/Landing/NavBar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex min-h-screen flex-col items-center justify-center bg-white py-2 dark:bg-black text-black dark:text-white ">
+        <div className="flex min-h-screen flex-col items-center justify-center bg-white py-2 text-black dark:bg-black dark:text-white">
             <Navbar />
-            <main>
-                {children}
-            </main>
+            <main>{children}</main>
 
             {/* FOOTER */}
             <footer className="mt-auto text-center text-xs text-gray-500 dark:text-gray-400">
