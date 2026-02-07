4782141da9526e6a77b0bb66e94571d15a361080
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-07 10:34:27 -0600

    feat: integrate book loading logic and enhance Libros layout with LibroCard component

--- src/pages/Libros.tsx
+++ src/pages/Libros.tsx
@@ -1,16 +1,25 @@
+import LibroCard from "@/components/Landing/LibroCard";
 import { useFiltersBooks } from "@/hooks/useFiltersLibro";
 import Layout from "@/layout/Layout";
+import { useBooksStore } from "@/store/libro.store";
+import { useEffect } from "react";
 
 export default function Libros() {
+    const { cargarLibros } = useBooksStore();
     const { books } = useFiltersBooks();
+
+    useEffect(() => {
+        cargarLibros();
+    }, [cargarLibros]);
     return (
         <Layout>
-            <div>
-                {books.map((book) => (
-                    <div key={book.id}>
-                        <h1>{book.volumeInfo.title}</h1>
-                    </div>
-                ))}
+            <div className="min-h-screen flex flex-col gap-5 bg-white  text-black dark:bg-black dark:text-white max-w-7xl py-10">
+                <h1 className="text-4xl font-bold py-5" >Catalogo de libros</h1>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-6 max">
+                    {books.map((book) => (
+                        <LibroCard key={book.id} book={book} />
+                    ))}
+                </div>
             </div>
         </Layout>
     );
