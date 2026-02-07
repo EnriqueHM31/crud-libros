4fcd979f25f32b3aa3f78a332987a59a11747ba0
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-07 00:32:47 -0600

    refactor: update Layout component to include Footer and adjust main section styling

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,15 +1,14 @@
+import Footer from "@/components/Landing/Footer";
 import Navbar from "@/components/Landing/NavBar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex min-h-screen flex-col items-center justify-center bg-white py-2 text-black dark:bg-black dark:text-white">
             <Navbar />
-            <main>{children}</main>
+            <main className="flex-1">{children}</main>
 
             {/* FOOTER */}
-            <footer className="mt-auto text-center text-xs text-gray-500 dark:text-gray-400">
-                <p>Acceso rápido solo para demostración</p>
-            </footer>
+            <Footer />
         </div>
     );
 }
