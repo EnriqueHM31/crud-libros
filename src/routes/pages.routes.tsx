9c9ca0d258d7f0354c4b13ef02c715671eb4566c
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-07 00:04:48 -0600

    feat: add new routes for Libros, Contacto, Favoritos, and Pedidos pages to enhance application navigation

--- src/routes/pages.routes.tsx
+++ src/routes/pages.routes.tsx
@@ -1,11 +1,16 @@
-import { Route, Routes } from "react-router-dom";
-import LoginDemo from "../pages/LoginDemo";
+
 import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
 import LoadingBooks from "../components/Atomos/Loading";
+import LoginDemo from "../pages/LoginDemo";
 
 const Dashboard = lazy(() => import("../pages/Dashboard"));
 const NotFound = lazy(() => import("../pages/NotFound"));
 const Landing = lazy(() => import("../pages/Landing"));
+const Pedidos = lazy(() => import("../pages/Pedidos"));
+const Libros = lazy(() => import("../pages/Libros"));
+const Contacto = lazy(() => import("../pages/Contacto"));
+const Favoritos = lazy(() => import("../pages/Favoritos"));
 
 export function PagesRoutes() {
     return (
@@ -19,7 +24,12 @@ export function PagesRoutes() {
             <Routes>
                 <Route path="/" element={<LoginDemo />} />
                 <Route path="/dashboard" element={<Dashboard />} />
+
                 <Route path="/usuario" element={<Landing />} />
+                <Route path="/libros" element={<Libros />} />
+                <Route path="/contacto" element={<Contacto />} />
+                <Route path="/favoritos" element={<Favoritos />} />
+                <Route path="/pedidos" element={<Pedidos />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </Suspense>
