a6602e343babea7a275ac0c26c76429883a3fdcb
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-07 00:32:50 -0600

    feat: integrate book loading functionality in Landing component

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,6 +1,15 @@
 import Layout from "@/layout/Layout";
+import { useBooksStore } from "@/store/libro.store";
+import { useEffect } from "react";
 
 export default function Landing() {
+
+    const { cargarLibros } = useBooksStore();
+
+    useEffect(() => {
+        cargarLibros();
+    }, [cargarLibros]);
+
     return (
         <Layout>
             <h1 className="text-primary-dark text-4xl font-bold">Bienvenido a la librería de libros</h1>
