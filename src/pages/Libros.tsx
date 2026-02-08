9e6327d801f5c88470653ff4fc88583ad1bced46
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-07 22:53:00 -0600

    refactor: improve layout and styling consistency in Libros page for better readability and user experience

--- src/pages/Libros.tsx
+++ src/pages/Libros.tsx
@@ -9,22 +9,21 @@ export default function Libros() {
     const { cargarLibros } = useBooksStore();
     const { books, total } = useFiltersBooks();
 
-
     useEffect(() => {
         cargarLibros();
     }, [cargarLibros]);
     return (
         <Layout>
-            <div className="flex min-h-screen max-w-7xl flex-col gap-5 bg-white  text-black dark:bg-black dark:text-white">
-                <div className="flex items-center justify-between gap-2 mt-8">
-                    <h1 className=" text-4xl font-bold">Catalogo de libros</h1>
+            <div className="flex min-h-screen max-w-7xl flex-col gap-5 bg-white text-black dark:bg-black dark:text-white">
+                <div className="mt-8 flex items-center justify-between gap-2">
+                    <h1 className="text-4xl font-bold">Catalogo de libros</h1>
 
-                    <p className="text-sm text-black font-bold dark:text-zinc-400 ">Mostrando {total} libros</p>
+                    <p className="text-sm font-bold text-black dark:text-zinc-400">Mostrando {total} libros</p>
                 </div>
 
                 <BooksFiltersLanding />
 
-                <div className="max grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 mt-7">
+                <div className="max mt-7 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
                     {books.map((book) => (
                         <LibroCard key={book.id} book={book} />
                     ))}
