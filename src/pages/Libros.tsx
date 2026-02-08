ddceac5abd0f4ca8cf88270068d938d0e63292a7
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-07 22:57:05 -0600

    refactor: update layout and add descriptive text in Libros page for improved user engagement and clarity

--- src/pages/Libros.tsx
+++ src/pages/Libros.tsx
@@ -15,12 +15,12 @@ export default function Libros() {
     return (
         <Layout>
             <div className="flex min-h-screen max-w-7xl flex-col gap-5 bg-white text-black dark:bg-black dark:text-white">
-                <div className="mt-8 flex items-center justify-between gap-2">
+                <div className="mt-15 flex items-center justify-between gap-2">
                     <h1 className="text-4xl font-bold">Catalogo de libros</h1>
-
                     <p className="text-sm font-bold text-black dark:text-zinc-400">Mostrando {total} libros</p>
                 </div>
 
+                <p className="max-w-4xl w-full text-black dark:text-gray-400 ">En esta página encontrarás el catálogo de libros disponibles en la librería HM. Aquí podrás explorar los títulos, filtrar por idioma o categoría y visualizar la información básica de cada libro para ubicarlo de forma rápida y sencilla.</p>
                 <BooksFiltersLanding />
 
                 <div className="max mt-7 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
