040f3f290bb8fabdcb299d87b53bc81f7fd2fd81
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-07 23:28:49 -0600

    feat: add Zustand store for managing favorite books with functionality to add, remove, and toggle read status

---
+++ src/store/favoritosLibros.store.ts
@@ -0,0 +1,69 @@
+import type { GoogleBook } from "@/types/libro";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+export interface LibroFav {
+    book: GoogleBook;
+    read: boolean;
+}
+
+interface FavoritosStore {
+    favoritos: LibroFav[]; // TODO: Cambiar nombre
+
+    agregarFavorito: (libro: GoogleBook) => void;
+    quitarFavorito: (id: string) => void;
+    toggleLeido: (id: string) => void;
+
+    esFavorito: (id: string) => boolean;
+    totalFavoritos: () => number;
+}
+
+export const useFavoritosStore = create<FavoritosStore>()(
+    persist(
+        (set, get) => ({
+            favoritos: [],
+
+            agregarFavorito: (libro) => {
+                console.log({ libro });
+                if (!libro || !libro.id) return;
+
+                const existe = get().favoritos.find(l => l.book.id === libro.id);
+                if (existe) return;
+
+                set({
+                    favoritos: [
+                        ...get().favoritos,
+                        { book: libro, read: false }
+                    ]
+                });
+            },
+
+
+            quitarFavorito: (id) => {
+                set({
+                    favoritos: get().favoritos.filter(l => l.book.id !== id)
+                });
+            },
+
+            toggleLeido: (id) => {
+                set({
+                    favoritos: get().favoritos.map(l =>
+                        l.book.id === id ? { ...l, read: !l.read } : l
+                    )
+                });
+            },
+
+            esFavorito: (id) => {
+                if (!id) return false;
+                return get().favoritos.some(l => l.book.id === id);
+            },
+
+            totalFavoritos: () => {
+                return get().favoritos.length;
+            }
+        }),
+        {
+            name: "libros-favoritos-storage"
+        }
+    )
+);
