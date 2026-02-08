70635b6537f174d992b85c655e71e3a92bbfe87b
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-07 22:52:36 -0600

    feat: display total number of books in Libros page and adjust layout for improved presentation

--- src/pages/Libros.tsx
+++ src/pages/Libros.tsx
@@ -7,7 +7,8 @@ import { useEffect } from "react";
 
 export default function Libros() {
     const { cargarLibros } = useBooksStore();
-    const { books } = useFiltersBooks();
+    const { books, total } = useFiltersBooks();
+
 
     useEffect(() => {
         cargarLibros();
@@ -15,11 +16,15 @@ export default function Libros() {
     return (
         <Layout>
             <div className="flex min-h-screen max-w-7xl flex-col gap-5 bg-white  text-black dark:bg-black dark:text-white">
-                <h1 className=" text-4xl font-bold">Catalogo de libros</h1>
+                <div className="flex items-center justify-between gap-2 mt-8">
+                    <h1 className=" text-4xl font-bold">Catalogo de libros</h1>
+
+                    <p className="text-sm text-black font-bold dark:text-zinc-400 ">Mostrando {total} libros</p>
+                </div>
 
                 <BooksFiltersLanding />
 
-                <div className="max grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
+                <div className="max grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 mt-7">
                     {books.map((book) => (
                         <LibroCard key={book.id} book={book} />
                     ))}
