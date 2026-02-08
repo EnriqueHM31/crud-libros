18c6c43558ba66ccf2cca7dfb454625671d5bca4
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-07 22:10:40 -0600

    refactor: improve error handling in Lenguajes component to provide more descriptive error messages

--- src/components/Dashboard/sections/Lenguajes.tsx
+++ src/components/Dashboard/sections/Lenguajes.tsx
@@ -22,7 +22,7 @@ export default function Lenguajes() {
             <FiltersLenguajes />
 
             {error ? (
-                <Error error={error} />
+                <Error error={error.message ?? "Ocurrió un error inesperado"} title={error.title ?? "Error"} />
             ) : !lenguajes || lenguajes.length === 0 ? (
                 <NotResults error="No se encontraron resultados para la categoría buscada" />
             ) : (
