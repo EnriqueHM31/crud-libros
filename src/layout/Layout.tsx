8ef0a353f0f68d1e6c4a8f2ee065179dc86ab80a
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-07 22:41:12 -0600

    refactor: remove unnecessary footer comment in Layout component for cleaner code

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -6,8 +6,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <div className="flex min-h-screen flex-col items-center justify-center bg-white py-2 text-black dark:bg-black dark:text-white">
             <Navbar />
             <main className="flex-1">{children}</main>
-
-            {/* FOOTER */}
             <Footer />
         </div>
     );
