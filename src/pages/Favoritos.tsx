d956056be2df948b2370ea9381de6c0524c46bda
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-08 21:13:59 -0600

    feat: integrate ModalLibro into Favoritos component for enhanced book detail viewing and manage modal state

--- src/pages/Favoritos.tsx
+++ src/pages/Favoritos.tsx
@@ -1,11 +1,22 @@
 import CardFavorito from "@/components/Landing/CardFavorito";
+import ModalLibro from "@/components/Landing/ModalLibro";
 import Layout from "@/layout/Layout";
 import { useFavoritosStore, type LibroFav } from "@/store/favoritosLibros.store";
+import type { GoogleBook } from "@/types/libro";
+import { useState } from "react";
 export default function Favoritos() {
     const { favoritos, totalFavoritos } = useFavoritosStore();
+    const [open, setOpen] = useState(false);
+    const [selected, setSelected] = useState<GoogleBook | null>(null);
+
+    const handleOpenModal = (book: GoogleBook) => {
+        setSelected(book);
+        setOpen(true);
+    };
 
     return (
         <Layout>
+            <ModalLibro open={open} onClose={() => setOpen(false)} book={selected} />
             {favoritos.length === 0 ? (
                 <section className="p-6">
                     <h1 className="mb-2 text-2xl font-bold">Favoritos</h1>
@@ -24,7 +35,7 @@ export default function Favoritos() {
 
                     <div className="mt-8 grid w-full items-center justify-center gap-10 md:grid-cols-2 lg:grid-cols-3">
                         {favoritos.map((libroFavorito: LibroFav) => (
-                            <CardFavorito key={libroFavorito.book.id} book={libroFavorito.book} read={libroFavorito.read} />
+                            <CardFavorito key={libroFavorito.book.id} book={libroFavorito.book} read={libroFavorito.read} onClickModal={handleOpenModal} />
                         ))}
                     </div>
                 </section>
