cbc207a1d9ed5fb8956ea45cb973f0d966107803
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-08 22:47:00 -0600

    refactor: change div to article in CardFavorito component for semantic HTML and improve button class formatting for better readability

--- src/components/Landing/CardFavorito.tsx
+++ src/components/Landing/CardFavorito.tsx
@@ -10,7 +10,7 @@ export default function CardFavorito({ book, read, onClickModal }: { book: Googl
     const isFavorito = esFavorito(book.id);
 
     return (
-        <div className="h-[550px] w-full rounded-2xl bg-zinc-800 p-4 shadow-sm shadow-zinc-300 outline -outline-offset-8 outline-zinc-600">
+        <article className="h-[550px] w-full rounded-2xl bg-zinc-800 p-4 shadow-sm shadow-zinc-300 outline -outline-offset-8 outline-zinc-600">
             <div className="group relative flex h-full w-full flex-col items-center justify-center overflow-hidden rounded-2xl bg-neutral-900 outline -outline-offset-8 outline-zinc-600 duration-500 before:absolute before:top-20 before:right-66 before:size-35 before:rounded-full before:bg-zinc-700 before:blur-xl before:duration-500 after:absolute after:-right-10 after:bottom-15 after:size-35 after:rounded-full after:bg-zinc-700 after:blur-xl after:duration-500 hover:rotate-8 hover:duration-500 hover:before:-translate-x-40 hover:before:translate-y-12 hover:after:translate-x-24 hover:after:duration-500">
                 <div className="z-10 flex h-full w-full flex-col items-center gap-2 px-7 py-3">
                     <header className="flex flex-4 flex-col items-center justify-center gap-2 text-center">
@@ -45,9 +45,8 @@ export default function CardFavorito({ book, read, onClickModal }: { book: Googl
                             <button
                                 title={read ? "Marcar libro como no leído" : "Marcar libro como leído"}
                                 onClick={() => toggleLeido(book.id)}
-                                className={`flex-1 cursor-pointer rounded-xl px-4 py-2 text-sm font-medium text-black transition-colors ${
-                                    read ? "bg-green-700 text-white hover:bg-green-800" : "bg-zinc-300 text-black hover:bg-zinc-500 hover:text-white"
-                                }`}
+                                className={`flex-1 cursor-pointer rounded-xl px-4 py-2 text-sm font-medium text-black transition-colors ${read ? "bg-green-700 text-white hover:bg-green-800" : "bg-zinc-300 text-black hover:bg-zinc-500 hover:text-white"
+                                    }`}
                             >
                                 {read ? "Marcar libro como no leído" : "Marcar libro como leído"}
                             </button>
@@ -55,6 +54,6 @@ export default function CardFavorito({ book, read, onClickModal }: { book: Googl
                     </section>
                 </div>
             </div>
-        </div>
+        </article>
     );
 }
