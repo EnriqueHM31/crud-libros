5be29b50ff1929ee8d3bd4d5628d38d47ffb4dfe
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-08 20:12:43 -0600

    refactor: enhance CardFavorito component layout and styling for improved readability and user experience

--- src/components/Landing/CardFavorito.tsx
+++ src/components/Landing/CardFavorito.tsx
@@ -14,48 +14,43 @@ export default function CardFavorito({ book, read }: { book: GoogleBook; read: b
     console.log({ read });
 
     return (
-        <div className="bg-zinc-800 rounded-2xl shadow-sm shadow-zinc-300 outline outline-zinc-600 -outline-offset-8 h-[550px] w-full p-4">
-            <div className="group overflow-hidden relative after:duration-500 before:duration-500  duration-500 hover:after:duration-500 hover:after:translate-x-24 hover:before:translate-y-12 hover:before:-translate-x-40 hover:duration-500 after:absolute after:size-35 after:bg-zinc-700 after:rounded-full  after:blur-xl after:bottom-15 after:-right-10 before:absolute before:bg-zinc-700 before:rounded-full  before:blur-xl before:top-20 before:right-66 before:size-35  hover:rotate-8   bg-neutral-900 rounded-2xl outline outline-zinc-600 -outline-offset-8 h-full w-full flex flex-col items-center justify-center">
-                <div className="z-10 flex flex-col items-center gap-2 w-full h-full px-7 py-3  ">
-
-                    <header className="flex-4 flex flex-col items-center justify-center gap-2 text-center ">
-
+        <div className="h-[550px] w-full rounded-2xl bg-zinc-800 p-4 shadow-sm shadow-zinc-300 outline -outline-offset-8 outline-zinc-600">
+            <div className="group relative flex h-full w-full flex-col items-center justify-center overflow-hidden rounded-2xl bg-neutral-900 outline -outline-offset-8 outline-zinc-600 duration-500 before:absolute before:top-20 before:right-66 before:size-35 before:rounded-full before:bg-zinc-700 before:blur-xl before:duration-500 after:absolute after:-right-10 after:bottom-15 after:size-35 after:rounded-full after:bg-zinc-700 after:blur-xl after:duration-500 hover:rotate-8 hover:duration-500 hover:before:-translate-x-40 hover:before:translate-y-12 hover:after:translate-x-24 hover:after:duration-500">
+                <div className="z-10 flex h-full w-full flex-col items-center gap-2 px-7 py-3">
+                    <header className="flex flex-4 flex-col items-center justify-center gap-2 text-center">
                         <div className="flex-1">
-
                             <img src={v.imageLinks?.thumbnail || "/no-image.jpg"} alt={v.title} className="h-60 w-full object-contain object-top" />
                         </div>
-                        <div className="flex-2 flex items-center justify-center gap-2 flex-col">
+                        <div className="flex flex-2 flex-col items-center justify-center gap-2">
+                            <span className="line-clamp-2 text-xl font-bold text-white">{v.title} </span>
 
-                            <span className="text-white text-xl font-bold line-clamp-2">{v.title} </span>
-
-                            <span className="text-gray-400 text-sm line-clamp-1">
-                                {v.subtitle}
-                            </span>
+                            <span className="line-clamp-1 text-sm text-gray-400">{v.subtitle}</span>
                         </div>
                     </header>
 
-                    <section className="flex gap-2 items-center flex-1 flex-col  w-full">
-                        <div className="flex gap-2 items-center justify-center w-full ">
-
+                    <section className="flex w-full flex-1 flex-col items-center gap-2">
+                        <div className="flex w-full items-center justify-center gap-2">
                             <button
                                 title={`Ver detalle de ${book.volumeInfo.title}`}
-                                className="rounded-xl flex-1 bg-zinc-300 transition-colors px-4 py-2 text-sm font-medium text-black hover:bg-zinc-500 hover:text-white cursor-pointer">
+                                className="flex-1 cursor-pointer rounded-xl bg-zinc-300 px-4 py-2 text-sm font-medium text-black transition-colors hover:bg-zinc-500 hover:text-white"
+                            >
                                 Ver detalle
                             </button>
                             <button
                                 title={isFavorito ? "Quitar de favoritos" : "Agregar a favoritos"}
-                                onClick={() => isFavorito ? quitarFavorito(book.id) : agregarFavorito(book)}
-                                className="rounded-xl flex-1 bg-zinc-300 transition-colors px-4 py-2 text-sm font-medium text-black hover:bg-zinc-500 hover:text-white cursor-pointer">
+                                onClick={() => (isFavorito ? quitarFavorito(book.id) : agregarFavorito(book))}
+                                className="flex-1 cursor-pointer rounded-xl bg-zinc-300 px-4 py-2 text-sm font-medium text-black transition-colors hover:bg-zinc-500 hover:text-white"
+                            >
                                 Quitar
                             </button>
                         </div>
-                        <div className="w-full flex">
+                        <div className="flex w-full">
                             <button
                                 title={read ? "Marcar libro como no leído" : "Marcar libro como leído"}
                                 onClick={() => toggleLeido(book.id)}
-                                className={`rounded-xl flex-1  transition-colors px-4 py-2 text-sm font-medium text-black  cursor-pointer 
-                                    ${read ? "bg-green-700 text-white hover:bg-green-800 " : "bg-zinc-300 text-black hover:bg-zinc-500 hover:text-white"
-                                    }`}
+                                className={`flex-1 cursor-pointer rounded-xl px-4 py-2 text-sm font-medium text-black transition-colors ${
+                                    read ? "bg-green-700 text-white hover:bg-green-800" : "bg-zinc-300 text-black hover:bg-zinc-500 hover:text-white"
+                                }`}
                             >
                                 {read ? "Marcar libro como no leído" : "Marcar libro como leído"}
                             </button>
@@ -64,6 +59,5 @@ export default function CardFavorito({ book, read }: { book: GoogleBook; read: b
                 </div>
             </div>
         </div>
-
     );
 }
