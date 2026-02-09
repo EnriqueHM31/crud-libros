8eede5a314420917a7565fdd5a609da20c1c604b
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-08 21:43:13 -0600

    feat: add new links with icons for user navigation in menu constants

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -1,6 +1,8 @@
 import type { MenuItem } from "@/types/store";
 import { FaHome, FaInfo, FaTags, FaBook, FaShoppingCart } from "react-icons/fa";
 import { HiLanguage } from "react-icons/hi2";
+import { FiHome, FiMail, FiPackage, FiStar, FiBook } from "react-icons/fi";
+
 export const menuItems = [
     { key: "home", name: "Home", icon: FaHome },
     { key: "informacion", name: "Información", icon: FaInfo },
@@ -9,3 +11,12 @@ export const menuItems = [
     { key: "libros", name: "Libros", icon: FaBook },
     { key: "pedidos", name: "Pedidos", icon: FaShoppingCart },
 ] as MenuItem[];
+
+
+export const links = [
+    { name: "Inicio", Icon: FiHome, href: "/usuario", private: false },
+    { name: "Libros", Icon: FiBook, href: "/usuario/libros", private: false },
+    { name: "Contacto", Icon: FiMail, href: "/usuario/contacto", private: false },
+    { name: "Favoritos", Icon: FiStar, href: "/usuario/favoritos", private: true },
+    { name: "Pedidos", Icon: FiPackage, href: "/usuario/pedidos", private: true },
+];
