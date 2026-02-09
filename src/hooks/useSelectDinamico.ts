e94990590a16038cbecbc574ebc3129f5f4dac58
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-08 22:51:58 -0600

    chore: remove console.log statements from useSelectDinamico hook for cleaner code

--- src/hooks/useSelectDinamico.ts
+++ src/hooks/useSelectDinamico.ts
@@ -7,15 +7,12 @@ export function useSelectDinamico({ value, handleChange }: { value: string[]; ha
     const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         const value = e.target.value;
 
-        console.log({ value });
         if (!value) return;
 
         // Si ya está en la lista, no hacemos nada
         if (selectedCategories.includes(value)) return;
-        console.log({ selectedCategories });
 
         const newSelected = [...selectedCategories, value];
-        console.log({ newSelected });
         setSelectedCategories(newSelected);
         handleChange(newSelected);
         setInputValue(newSelected.join(", "));
