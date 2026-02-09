d94bd37002917973b0374ed984e42bd1159f54cd
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-08 22:52:12 -0600

    chore: remove console.log statement from editBook function for cleaner code

--- src/store/libro.store.ts
+++ src/store/libro.store.ts
@@ -121,7 +121,6 @@ export const useBooksStore = create<BooksState>((set) => ({
     },
 
     editBook: async (id, updatedFields) => {
-        console.log({ updatedFields });
         const { data, message } = (await updateBook(updatedFields, id)) as { data: GoogleBook; message: string };
 
         toast.success(message ?? "Libro actualizado correctamente");
@@ -129,25 +128,25 @@ export const useBooksStore = create<BooksState>((set) => ({
             books: state.books.map((book) =>
                 book.id === id
                     ? {
-                          ...book,
-                          ...data,
-                          volumeInfo: {
-                              ...book.volumeInfo,
-                              ...data.volumeInfo,
-                          },
-                      }
+                        ...book,
+                        ...data,
+                        volumeInfo: {
+                            ...book.volumeInfo,
+                            ...data.volumeInfo,
+                        },
+                    }
                     : book
             ),
             selectedBook:
                 state.selectedBook?.id === id
                     ? {
-                          ...state.selectedBook,
-                          ...data,
-                          volumeInfo: {
-                              ...state.selectedBook.volumeInfo,
-                              ...data.volumeInfo,
-                          },
-                      }
+                        ...state.selectedBook,
+                        ...data,
+                        volumeInfo: {
+                            ...state.selectedBook.volumeInfo,
+                            ...data.volumeInfo,
+                        },
+                    }
                     : state.selectedBook,
             modalMode: "view",
         }));
