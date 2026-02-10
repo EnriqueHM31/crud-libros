c9954e30a001cfaa2378460eaf719870fc5b3f80
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-09 19:48:48 -0600

    refactor: rename authentication functions and update API endpoints for consistency and clarity

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,7 +5,7 @@ export interface LoginResponse {
     };
 }
 
-export async function loginService(username: string, password: string): Promise<LoginResponse> {
+export async function registrarUsuario(username: string, password: string): Promise<LoginResponse> {
     const res = await fetch(import.meta.env.VITE_API_URL_AUTH + "/login", {
         method: "POST",
         headers: {
@@ -23,8 +23,26 @@ export async function loginService(username: string, password: string): Promise<
     return res.json();
 }
 
-export async function logoutService(): Promise<void> {
-    const res = await fetch(import.meta.env.VITE_API_URL_AUTH + "/logout", {
+export async function IniciarSesion(username: string, password: string): Promise<LoginResponse> {
+    const res = await fetch(import.meta.env.VITE_API_URL_AUTH + "/iniciar-sesion", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: "include", // clave para cookies
+        body: JSON.stringify({ username, password }),
+    });
+
+    if (!res.ok) {
+        const data = await res.json().catch(() => null);
+        throw new Error(data?.message || "Error en login");
+    }
+
+    return res.json();
+}
+
+export async function CerrarSesion(): Promise<void> {
+    const res = await fetch(import.meta.env.VITE_API_URL_AUTH + "/cerrar-sesion", {
         method: "POST",
         credentials: "include",
     });
@@ -34,8 +52,8 @@ export async function logoutService(): Promise<void> {
     }
 }
 
-export async function meService() {
-    const res = await fetch(import.meta.env.VITE_API_URL_AUTH + "/me", {
+export async function obtenerUsuario() {
+    const res = await fetch(import.meta.env.VITE_API_URL_AUTH + "/usuario", {
         method: "GET",
         credentials: "include",
     });
