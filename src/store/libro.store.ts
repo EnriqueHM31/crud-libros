ccd7a93e1e2d9958fb2f52208d2182fda6864bc2
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-09 22:33:28 -0600

    refactor: improve code formatting in libro.store.ts for enhanced readability

--- src/store/libro.store.ts
+++ src/store/libro.store.ts
@@ -128,25 +128,25 @@ export const useBooksStore = create<BooksState>((set) => ({
             books: state.books.map((book) =>
                 book.id === id
                     ? {
-                        ...book,
-                        ...data,
-                        volumeInfo: {
-                            ...book.volumeInfo,
-                            ...data.volumeInfo,
-                        },
-                    }
+                          ...book,
+                          ...data,
+                          volumeInfo: {
+                              ...book.volumeInfo,
+                              ...data.volumeInfo,
+                          },
+                      }
                     : book
             ),
             selectedBook:
                 state.selectedBook?.id === id
                     ? {
-                        ...state.selectedBook,
-                        ...data,
-                        volumeInfo: {
-                            ...state.selectedBook.volumeInfo,
-                            ...data.volumeInfo,
-                        },
-                    }
+                          ...state.selectedBook,
+                          ...data,
+                          volumeInfo: {
+                              ...state.selectedBook.volumeInfo,
+                              ...data.volumeInfo,
+                          },
+                      }
                     : state.selectedBook,
             modalMode: "view",
         }));
